31deacfaa3f87a4f1f807a74d93b6323f6eb5a60
nishitagiduturi <nishitaoct26@gmail.com>
2020-12-01 16:08:23 +0530

    Added recipe ingredients model, implemented shopping list component

--- ng-handson/src/app/shopping-list/shopping-list.component.ts
+++ ng-handson/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 15),
+    new Ingredient('Lemons', 25),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
